31b1227cf7fd094a5aa6f512f43f0650d24aa00e
Wilbur Han <wilbur.han@gmail.com>
2016-01-04 21:52:23 -0800

    errors wtf

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Post = mongoose.model('Post')
+var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
 /* GET home page. */
@@ -14,7 +14,7 @@ module.exports = router;
 /* GET posts page. */
 router.get('/posts', function(req, res, next) {
 	Post.find(function(err, posts){
-		if(err){return next(err);}
+		if(err){ return next(err); }
 		res.json(posts);
 	});
 });
@@ -24,7 +24,7 @@ router.post('/posts', function(req, res, next){
 	var post = new Post(req.body);
 	
 	post.save(function(err, post){
-		if(err){ return next(err);}
+		if(err){ return next(err); }
 		res.json(post);
 	});
 });
